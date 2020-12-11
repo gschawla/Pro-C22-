7ba240148428dff1b630650d0a11866014e65426
gschawla <74096480+gschawla@users.noreply.github.com>
2020-12-11 17:42:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,75 @@
-var wall, thickness , bullet, speed, weight;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-
-
-function setup(){
-  createCanvas(1600,800);
-thickness=random(22,83);
-wall=createSprite(1200,200,thickness,height/2)
-speed=random(223,321);
-weight=random(30,52)
-bullet=createSprite(50,200,50,5)
-bullet.velocityX=speed;
-
-}
-
-function draw (){
-
-if (hasCollided(bullet,wall)){
- 
-
-var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness)
-}
-if (damage>10){
-  wall.shapeColor=color(255,0,0)
-}
-
-if(damage<10){
-
-  wall.shapeColor=color(0,255,0)
-}
-
-drawSprites();
-
-}
-
-function hasCollided(bullet,wall){
-
-bulletRightEdge= bullet.x + bullet.width;
-
-wallLeftEdge=wall.x;
-if(bulletRightEdge>=wallLeftEdge)
-
+function preload()
 {
-return true ;
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-return false;
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
+	World.add(world, packageBody)
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  var packageSprite_options={
+	isStatic: true
+  }
+
 }
 
 
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  Engine.update(engine);
+  packageSprite.x=packageBody.position.x
+  packageSprite.y=packageBody.position.y
+ 
+  
 
 
+  drawSprites();
+}
 
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+ Matter.Body.setStatic(packageBody,false)
+   
+ //packageSprite_options;
 
-
-
-
-
+  }
+}
 
 
 
